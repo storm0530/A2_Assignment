0f209b70b51bfe595b1f02508bd13c8dcc4eb7e6
fukui <fukui.arashi@ist.osaka-u.ac.jp>
2021-04-23 15:32:49 +0000

    modified

--- Missing_data_completion.py
+++ Missing_data_completion.py
@@ -7,14 +7,17 @@ import sys
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-DUMMY_FLAG = 0 # 1: Use dummy variables, 0: Don't use
-POSITION_FLAG = 0 # 1: Use Position value, 0: Don't use
+DUMMY_FLAG = 1 # 1: Use dummy variables, 0: Don't use
+
+# select header for explanatory variable
+HEADER_FOR_ANALYZE = ["Gender","Age","Drink","Total_Cholesterol(mg/dL)"]
+# all header is ["Name","Gender","Age","Position","BMI","Drink","Smoke","Total_Cholesterol(mg/dL)"]
 
 # Function to find the response variable
 def predict_y(X, coef, header):
     # X(dictionary type) : explanatory variable
     # coef : coefficients of the regression equation
-    # header : headers for analyze
+    # header : header for explanatory variable
     
     y = coef["const"]
     for i in header:
@@ -24,17 +27,15 @@ def predict_y(X, coef, header):
 
 
 
+
 # Dictionary for conversion strings to numbers as appropriate
-if POSITION_FLAG == 0:
-    numeric_dict = {"Female":0,"Male":1,"No":0,"Yes":1,"Occasionally":1,"Often":2}
-else:
-    numeric_dict = {"Female":0,"Male":1,"No":0,"Yes":1,"Occasionally":1,"Often":2,
-    "Assistant Professor":1,"Associate Professor":2,"Professor":3
-    }
-
-# setting header list
-header = ["Name","Gender","Age","Position","BMI","Drink","Smoke","Total_Cholesterol(mg/dL)"]
-
+numeric_dict = {
+    "Female":0,"Male":1,"No":0,"Yes":1,
+    "Occasionally":1,"Often":2,
+    "Assistant Professor":1,
+    "Associate Professor":2,
+    "Professor":3
+}
 
 # load from csv
 df = pd.read_csv("blood_test_data.csv")
@@ -42,49 +43,39 @@ pd.set_option('display.max_rows', None)
 
 # Numeric_conversion
 for i in range(len(df)):
-    for j in header:
+    for j in HEADER_FOR_ANALYZE:
         if df.at[i,j] in numeric_dict:
             df.at[i,j] = numeric_dict[df.at[i,j]]
 
 # save data after conversion
 df.to_csv("./blood_test_data_edited.csv")
 
-# Not use dummy variables
-if DUMMY_FLAG == 0:
+# Extract only header values used for analysis
+if DUMMY_FLAG == 1:
+    # make dummy variables
+    HEADER_FOR_ANALYZE.append("BMI")
+    df = pd.get_dummies(df[HEADER_FOR_ANALYZE]).astype(float)
     
-    # Not use position value
-    if POSITION_FLAG == 0:
-        header_for_analyze = ["Gender","Age","Drink","Smoke","Total_Cholesterol(mg/dL)"]
-    
-    # Use position value
-    else:
-        header_for_analyze = ["Gender","Age","Position","Drink","Smoke","Total_Cholesterol(mg/dL)"]
-
-# Use dummy variables
+    # add dummy to header selected for explanatory variable
+    HEADER_FOR_ANALYZE = list(df)
 else:
-    df = pd.get_dummies(df[["Gender","Age","Position","BMI","Drink","Smoke","Total_Cholesterol(mg/dL)"]])
-    
-    # Not use position value
-    if POSITION_FLAG == 0:
-        header_for_analyze = ["Gender_0","Gender_1","Age","Drink_0","Drink_1","Drink_2","Smoke_0","Smoke_1","Total_Cholesterol(mg/dL)"]
-        
-    # Use position value
-    else:
-        header_for_analyze = ["Gender_0","Gender_1","Age","Position_1","Position_2","Position_3","Drink_0","Drink_1","Drink_2","Smoke_0","Smoke_1","Total_Cholesterol(mg/dL)"]
+    HEADER_FOR_ANALYZE.append("BMI")
+    df = df[HEADER_FOR_ANALYZE].astype(float)
+
+HEADER_FOR_ANALYZE.remove("BMI")
 
 
 # separate the "Ada" data for training and the other data for testing
 df_train = df.iloc[1:,:]
 df_test = df.iloc[:1,:]
 
-# Extract only header values used for analysis
-x_train = (df_train[header_for_analyze]).astype(float)
-X_test = (df_test[header_for_analyze]).astype(float)
-
+# X: explanatory variables, Y: response variable
+X_train = (df_train[HEADER_FOR_ANALYZE]).astype(float)
+X_test = (df_test[HEADER_FOR_ANALYZE]).astype(float)
 Y_train = (df_train['BMI']).astype(float)
 
 # add a constant term to the regression equation
-X_train = sm.add_constant(x_train)
+X_train = sm.add_constant(X_train)
 
 # transform into dictionary type
 X_test = X_test.to_dict()
@@ -94,7 +85,6 @@ X_test = {i: X_test[i][0] for i in X_test}
 model = sm.OLS(Y_train, X_train)
 result = model.fit()
 
-
 # display the results of multiple regression analysis
 print(result.summary())
 
@@ -102,12 +92,11 @@ print(result.summary())
 result_coef = result.params
 
 # Prediction of missing BMI values
-predict = predict_y(X_test,result_coef,header_for_analyze)
+predict = predict_y(X_test,result_coef,HEADER_FOR_ANALYZE)
 
 # save results of multiple regression analysis and prediction
-with open('result_summary.txt', 'w') as f:
+with open('result.txt', 'w') as f:
     print(f"{result.summary()}\n\n",file=f)
     print(f"According to the multiple regression analysis by Arashi Fukui,\n\
 Ada's BMI is about {predict}.",file=f)
-
 # Ada's BMI is about 24.4609...
